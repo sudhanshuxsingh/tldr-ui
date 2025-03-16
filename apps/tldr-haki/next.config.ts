b840778c9c0de035b1185193e136ebd402cf7ec1
s2-sengar <sudhanshu.iem2k18@gmail.com>
2025-03-16 17:39:57 +0530

    feat(tldr/haki): add env variable check before build

--- apps/tldr-haki/next.config.ts
+++ apps/tldr-haki/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+import "@/env/server";
+
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
