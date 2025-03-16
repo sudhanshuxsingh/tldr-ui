c2a5c73de9442beb4299efc50d3d9e0484f794db
s2-sengar <sudhanshu.iem2k18@gmail.com>
2025-03-16 11:35:44 +0530

    feat: add shortcut support to login btn

--- apps/tldr-haki/src/components/ui/auth/login-button.tsx
+++ apps/tldr-haki/src/components/ui/auth/login-button.tsx
@@ -1,14 +1,35 @@
+"use client"
+
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginButton() {
+    const router = useRouter();
+    useEffect(() => {
+        router.prefetch("/auth/login");
+        const down = (e: KeyboardEvent) => {
+            if (e.key.toLowerCase() === "l") {
+                e.preventDefault()
+                router.push("/auth/login");
+            }
+        }
+        document.addEventListener("keydown", down)
+        return () => document.removeEventListener("keydown", down)
+    }, [])
+
     return (
-        <Link href="/auth/login" className={cn(buttonVariants({ variant: "ghost" }), "items-center text-sm text-muted-foreground")}>
-            <span>Login</span>
-            <kbd className="pointer-events-none inline-flex select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                L
-            </kbd>
-        </Link>
+        <Button asChild variant="ghost" className="group/login-btn gap-1">
+            <Link href="/auth/login">
+                <span>Login</span>
+                <kbd className="pointer-events-none inline-flex select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100 group-hover/login-btn:bg-background">
+                    L
+                </kbd>
+            </Link>
+        </Button>
     )
 }
+
+// className={cn(buttonVariants({ variant: "ghost" }), "items-center text-sm text-muted-foreground")}
